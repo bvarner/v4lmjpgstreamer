eb9697868c0a7262e946715172fffae5c0a99eea
Bryan Varner <1652015+bvarner@users.noreply.github.com>
2020-03-24 15:36:49 -0400

    Added SSL Config / Handling

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
 var camera *Camera;
+
+func redirectTLS(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "https://" + r.Host + r.RequestURI, http.StatusMovedPermanently)
+}
 
 func main() {
 	var err error = nil
@@ -37,6 +43,20 @@ func main() {
 	// TODO: REST API For video device control
 	http.HandleFunc("/", camera.ServeHTTP)
 
-	// TODO: Cert & Key
-	log.Fatal(http.ListenAndServe(":80", nil))
+	cert := flag.String("cert", "/etc/ssl/certs/v4lmjpgstreamer.pem", "The certificate for this server.")
+	certkey := flag.String("key", "/etc/ssl/certs/v4lmjpgstreamer-key.pem", "The key for the server cert.")
+
+	_, certerr := os.Stat(*cert)
+	_, keyerr := os.Stat(*certkey)
+
+	if certerr == nil && keyerr == nil {
+		fmt.Println("SSL Configuration set up.")
+		go func() {
+			log.Fatal(http.ListenAndServe(":80", http.HandlerFunc(redirectTLS)));
+		} ()
+		log.Fatal(http.ListenAndServeTLS(":443", *cert, *certkey, nil))
+	} else {
+		fmt.Println("SSL Configuration not found.")
+		log.Fatal(http.ListenAndServe(":80", nil))
+	}
 }
